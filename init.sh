8afc99e86ceda691bf60e6404e9f6c7e56ada8a0
temon935 <temon935@gmail.com>
2020-08-21 11:08:43 +0300

    version 1.0.5

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ sudo /etc/init.d/nginx restart
 
 
 sudo rm -r /etc/gunicorn.d/*
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test.conf
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
 sudo /etc/init.d/gunicorn restart
