1a6a9dfcc87aacb67d578f2aba6cadb9eb473989
temon935 <temon935@gmail.com>
2020-08-31 11:07:21 +0300

    new version

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo rm -r /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
